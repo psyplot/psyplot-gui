b2cfd4ef2ff3da769b9ac735ced44a1a2850720b
Chilipp <p.sommer@posteo.de>
2016-04-21 22:48:33 +0200

    Updated to new version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readme():
 
 
 setup(name='psyplot_gui',
-      version='0.0.3.dev5',
+      version='0.0.3.dev6',
       description='Graphical user interface for the psyplot package',
       long_description=readme(),
       classifiers=[
