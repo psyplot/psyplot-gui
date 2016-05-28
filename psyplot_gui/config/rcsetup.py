fa910f51af85a9e8c2f6a27c1a02ba6a7b415c5e
Chilipp <p.sommer@posteo.de>
2016-05-28 21:57:06 +0200

    Use GuiRcParams for rcParams with changed base filename

--- psyplot_gui/config/rcsetup.py
+++ psyplot_gui/config/rcsetup.py
@@ -67,7 +67,7 @@ defaultParams = {
 
 #: :class:`~psyplot.config.rcsetup.RcParams` instance that stores default
 #: formatoptions and configuration settings.
-rcParams = RcParams()
+rcParams = GuiRcParams()
 rcParams.defaultParams = defaultParams
 rcParams.update({key: val[0] for key, val in defaultParams.items()})
 rcParams.load_from_file()
