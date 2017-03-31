659273b57235cd8dd80b146a5de9cd81401b40b5
Chilipp <p.sommer@posteo.de>
2017-03-31 14:22:02 +0200

    Added Mac App files

---
+++ psyplot_gui/app/Psyplot.app/Contents/MacOS/run.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# if psyplot-bins.txt is existent, loop through it until an existing binary is
+# found
+if [[ -e $HOME/.config/psyplot/psyplot-bins.txt ]]; then
+    while IFS='' read -r PREFIX; do
+        if [[ $PREFIX != '' ]]; then
+            BIN=$PREFIX/bin/psyplot
+            if [[ -e $BIN ]]; then
+                break
+            fi
+        fi
+    done < $HOME/.config/psyplot/psyplot-bins.txt
+    source activate $PREFIX
+    $BIN -pwd $HOME
+else  # otherwise assume psyplot is installed in default python
+    python -m psyplot
+fi
