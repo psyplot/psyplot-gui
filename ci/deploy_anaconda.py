5605d6f4c6b2807a9cbfa6a52c2756cf82813878
Chilipp <p.sommer@posteo.de>
2017-07-29 12:55:03 +0200

    Fixed bugs for anaconda upload

--- ci/deploy_anaconda.py
+++ ci/deploy_anaconda.py
@@ -5,19 +5,32 @@ This script is designed for appveyor to deploy the build to anaconda cloud
 using the token from the CONDA_REPO_TOKEN environment variable"""
 import os.path as osp
 import os
+import six
 import glob
 import subprocess as spr
+
+if six.PY2:
+    FileExistsError = OSError
 
 fname = spr.check_output(
     ['conda', 'build', osp.join('ci', 'conda_recipe'), '--output',
      '--python', os.getenv('PYTHON_VERSION')]).decode(
-        'utf-8').strip()
+        'utf-8').strip().splitlines()[-1]
 
-spr.check_call(['conda', 'convert', fname, '-p', 'win-32'])
+try:
+    os.makedirs('builds')
+except FileExistsError:  # directory exists already
+    pass
+
+spr.check_call(['conda', 'convert', fname, '-p', 'builds'])
+
+files = [fname] + glob.glob(osp.join('builds', 'win-32', '*'))
+
+print('Uploading %s' % files)
 
 p = spr.Popen(
-    ['anaconda', '-t', os.getenv('CONDA_REPO_TOKEN'), 'upload', fname] +
-    glob.glob(osp.join('win-32', '*')), stdout=spr.PIPE, stderr=spr.PIPE)
+    ['anaconda', '-t', os.getenv('CONDA_REPO_TOKEN'), 'upload'] + files,
+    stdout=spr.PIPE, stderr=spr.PIPE)
 
 p.wait()
 
