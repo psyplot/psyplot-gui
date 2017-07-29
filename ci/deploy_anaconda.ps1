1be8e9eb242925f2dc61f0915f2446e39d0a84ae
Chilipp <p.sommer@posteo.de>
2017-07-29 22:27:41 +0200

    Added conversion to other architecture

--- ci/deploy_anaconda.ps1
+++ ci/deploy_anaconda.ps1
@@ -4,19 +4,41 @@
 function UploadToAnaconda ($architecture, $python_home) {
     Write-Host "Uploading files for" $architecture "from" $python_home
 
+    if ($architecture -match "32") {
+        $architecture2 = "64"
+    } else {
+        $architecture2 = "32"
+    }
+
     $basedir = $pwd.Path + "\"
     $filepath = $python_home + "\conda-bld\win-" + $architecture + "\psyplot-gui-*-py*.tar.bz2"
     $filepath = Resolve-Path $filepath
-    Write-Host "Uploading" $filepath
-    $anaconda_path = $python_home + "\Scripts\anaconda.exe"
-    $args = "-t " + $env:CONDA_REPO_TOKEN +" upload " + $filepath
-    Write-Host $filepath $args
-    Start-Process -FilePath $anaconda_path -ArgumentList $args -Wait -Passthru
-    if (Test-Path $python_home) {
-        Write-Host "Upload complete"
-    } else {
+    $conda_path = $python_home + "\Scripts\conda.exe"
+
+    Write-Host "Converting to win-" + $architecture2
+    $args = "-p " + "win-" + $architecture2 + " $filepath"
+    Write-Host $conda_path $args
+    $proc = (Start-Process -FilePath $conda_path -ArgumentList $args -Wait -Passthru)
+    if ($proc.ExitCode -ne 0) {
         Write-Host "Failed."
         Exit 1
+    } else {
+        Write-Host "Upload complete"
+    }
+
+    $filepath2 = $basedir + "win-" + $architecture2 + "\psyplot-gui*.tar.bz2"
+    $filepath2 = Resolve-Path $filepath2
+
+    Write-Host "Uploading" $filepath
+    $anaconda_path = $python_home + "\Scripts\anaconda.exe"
+    $args = "-t " + $env:CONDA_REPO_TOKEN +" upload " + $filepath + " " + $filepath2
+    Write-Host $anaconda_path $args
+    $proc = (Start-Process -FilePath $anaconda_path -ArgumentList $args -Wait -Passthru)
+    if ($proc.ExitCode -ne 0) {
+        Write-Host "Failed."
+        Exit 1
+    } else {
+        Write-Host "Upload complete"
     }
 }
 
