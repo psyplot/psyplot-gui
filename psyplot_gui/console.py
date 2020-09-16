7891de25e972990704023bfe5fe8f2bc43f31ba7
Philipp S. Sommer <philipp.sommer@hzg.de>
2020-09-16 16:26:28 +0200

    patch incompatibility between tornado and asyncio
    see https://github.com/ipython/ipykernel/pull/480
    
    closes https://github.com/psyplot/psyplot-gui/issues/18

--- psyplot_gui/console.py
+++ psyplot_gui/console.py
@@ -48,6 +48,34 @@ symbols_patt = re.compile(r"[^\'\"a-zA-Z0-9_.]")
 logger = logging.getLogger(__name__)
 
 
+def init_asyncio_patch():
+    """set default asyncio policy to be compatible with tornado
+    Tornado 6 (at least) is not compatible with the default
+    asyncio implementation on Windows
+    Pick the older SelectorEventLoopPolicy on Windows
+    if the known-incompatible default policy is in use.
+    do this as early as possible to make it a low priority and overrideable
+    ref: https://github.com/tornadoweb/tornado/issues/2608
+    FIXME: if/when tornado supports the defaults in asyncio,
+           remove and bump tornado requirement for py38
+    """
+    if sys.platform.startswith("win") and sys.version_info >= (3, 8):
+        import asyncio
+        try:
+            from asyncio import (
+                WindowsProactorEventLoopPolicy,
+                WindowsSelectorEventLoopPolicy,
+            )
+        except ImportError:
+            pass
+            # not affected
+        else:
+            if type(asyncio.get_event_loop_policy()) is WindowsProactorEventLoopPolicy:
+                # WindowsProactorEventLoopPolicy is not compatible with tornado 6
+                # fallback to the pre-3.8 default of Selector
+                asyncio.set_event_loop_policy(WindowsSelectorEventLoopPolicy())
+
+
 class IPythonControl(QTextEdit):
     """A modified control to show the help of objects in the help explorer"""
 
@@ -93,6 +121,12 @@ class ConsoleWidget(QtInProcessRichJupyterWidget, DockMixin):
             :class:`qtconsole.rich_jupyter_widget.RichJupyterWidget`
         """
         self._closed = False
+
+        # Create an in-process kernel
+        # >>> print_process_id()
+        # will print the same process ID as the main process
+        init_asyncio_patch()
+
         kernel_manager = QtInProcessKernelManager()
         # on windows, sys.stdout may be None when using pythonw.exe. Therefore
         # we just us a StringIO for security
