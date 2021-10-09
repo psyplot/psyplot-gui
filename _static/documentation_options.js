e181641a1746411c5e62c4e1b1f04b7e6cdc81df
ci-build <ci-build@psyplot.org>
2021-10-09 15:03:04 +0000

    [skip ci] CircleCi Build 293, commit 227d84ea8ebe932ef91e58d2ceb02f2a936d38d7

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.3.0+10.gbca2600',
+    VERSION: '1.3.0+11.g227d84e',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
